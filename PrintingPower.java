5d85e59836352c919059e72fa42390cbcbd36c0d
NewGuy250 <ndaniel220@yahoo.com>
2016-11-05 18:32:37 -0400

    Add files via upload

---
+++ PrintingPower.java
@@ -0,0 +1,13 @@
+
+public class PrintingPower {
+	public static void main(String[] args) {
+		//Let's print a huge smiley face!
+		System.out.println("XXXXXXXXXXX");
+		System.out.println("X  _   _  X");
+		System.out.println("X  0   0  X");
+		System.out.println("X    ^    X");	
+		System.out.println("X  \\___/  X");
+		System.out.println("X         X");
+		System.out.println("XXXXXXXXXXX");
+	}
+}
