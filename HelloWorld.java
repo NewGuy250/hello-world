c46bd4b5f52f02aee731c9c113030e4f18232e56
NewGuy250 <ndaniel220@yahoo.com>
2016-11-05 17:37:17 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+	/*
+	 * main() function executes when Java program is run
+	 * public = accessible to all
+	 * static = does not need to be declared to be used
+	 * void = returns nothing
+	 */
+	public static void main(String[] args) {
+		//This is a comment
+		System.out.println("Hello, World!!!!!");
+	}
+}
